ba35b638ef836d1f02af037e5955a5b5b57ef3e9
davSystem <fada60736@gmail.com>
2026-05-22 19:29:33 -0600

    Update discord-config.ts

--- src/lib/discord-config.ts
+++ src/lib/discord-config.ts
@@ -4,6 +4,6 @@ export const ROLE_ID_TRABAJADOR = "1504691022184059011";
 export const ROLE_ID_ADMIN = "1504691072226431076";
 
 // 👇 PEGA AQUÍ el ID del rol "Policía" de tu servidor:
-export const ROLE_ID_POLICIA = "REEMPLAZAR_ROLE_ID_POLICIA";
+export const ROLE_ID_POLICIA = "1507522602808377435";
 
 export const DISCORD_OAUTH_SCOPES = "identify guilds.members.read";
